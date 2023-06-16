2638c03736af622bf3928cceb39ab9071f1707fd
ThePixelator0 <jordanjohnswork@gmail.com>
2023-06-16 11:18:54 -0700

    Init springboot

---
+++ src/test/java/com/cmpt276/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
