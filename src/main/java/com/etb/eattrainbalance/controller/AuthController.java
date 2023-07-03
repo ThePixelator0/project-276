7cb0f24a7c3d942c46f88aa53b73953c3d4ca8be
ThePixelator0 <jordanjohnswork@gmail.com>
2023-07-02 18:32:32 -0700

    Fixed redirect issue on login

--- src/main/java/com/etb/eattrainbalance/controller/AuthController.java
+++ src/main/java/com/etb/eattrainbalance/controller/AuthController.java
@@ -9,6 +9,8 @@ import com.etb.eattrainbalance.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,48 +18,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-
     private final AuthService authenticate;
 
-    // This method handles HTTP POST requests to /api/auth/register for user registration.
-    /*@PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticate.register(request));
-    }*/
-
     @PostMapping("/register")
-    public String register(@Valid @RequestParam("name") String name, @Valid @RequestParam("email") String email, @Valid @RequestParam("password") String password, HttpServletRequest request) {
+    public ResponseEntity<Void> register(@Valid @RequestParam("name") String name, @Valid @RequestParam("email") String email, @Valid @RequestParam("password") String password, HttpServletRequest request) {
         RegisterRequest registerRequest = new RegisterRequest();
         registerRequest.setName(name);
         registerRequest.setEmail(email);
         registerRequest.setPassword(password);
         ResponseEntity.ok(authenticate.register(registerRequest, request));
-        return "redirect:/dashboard";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/dashboard");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
     @PostMapping("/register-admin")
-    public String registerAdmin(@Valid @RequestParam("name") String name, @Valid @RequestParam("email") String email, @Valid @RequestParam("password") String password, HttpServletRequest request) {
+    public ResponseEntity<Void> registerAdmin(@Valid @RequestParam("name") String name, @Valid @RequestParam("email") String email, @Valid @RequestParam("password") String password, HttpServletRequest request) {
         RegisterRequest registerRequest = new RegisterRequest();
         registerRequest.setName(name);
         registerRequest.setEmail(email);
         registerRequest.setPassword(password);
         ResponseEntity.ok(authenticate.registerAdmin(registerRequest, request));
-        return "redirect:/dashboard";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/dashboard");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
     // This method handles HTTP POST requests to /api/auth/login for user login.
     @PostMapping("/login")
-    public String login(@Valid @RequestParam("email") String email, @RequestParam("password") String password, final HttpServletRequest request) {
+    public ResponseEntity<Void> login(@Valid @RequestParam("email") String email, @RequestParam("password") String password, final HttpServletRequest request) {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setEmail(email);
         loginRequest.setPassword(password);
         ResponseEntity.ok(authenticate.login(loginRequest, request));
-        return "redirect:/dashboard";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/dashboard");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
 }
