4e54892a2a44058fed6a3148f0e8cf5aa7ccdaa2
philakagujones <philakagujones@gmail.com>
2023-07-03 13:16:48 -0500

    Update WorkoutRepository.java

--- src/main/java/com/etb/eattrainbalance/models/WorkoutRepository.java
+++ src/main/java/com/etb/eattrainbalance/models/WorkoutRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public class WorkoutRepository extends JpaRepository<Workouts,Integer> {
-    List<Workouts> ;
+    List<Workouts> findByWorkoutType(String workoutType);
 }
