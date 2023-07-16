c084a8c7efc943b76dc92d8dd28230ed1356a14c
bevannutakor <bevannutakor@icloud.com>
2023-07-15 20:50:02 -0700

    food form
    todo: backend api request

--- src/main/resources/static/js/nutritionscript.js
+++ src/main/resources/static/js/nutritionscript.js
@@ -3,35 +3,12 @@ const currDate = Date.now()
 const dt = new Date(currDate)
 document.getElementById("nutrition-date").innerHTML = dt.toDateString()
 
-function openBreakfastData(){
-    document.getElementById("breakfast-modal").style.display = "block"
+/*get and add data*/
+function openNutritionData(modalId){
+    document.getElementById(modalId).style.display = "block"
 }
 
-function closeBreakfastData(){
-    document.getElementById("breakfast-modal").style.display = "none"
+function closeNutritionData(modalId){
+    document.getElementById(modalId).style.display = "none"
 }
 
-
-function openLunchData(){
-    document.getElementById("lunch-modal").style.display = "block"
-}
-
-function closeLunchData(){
-    document.getElementById("lunch-modal").style.display = "none"
-}
-
-function openDinnerData(){
-    document.getElementById("dinner-modal").style.display = "block"
-}
-
-function closeDinnerData(){
-    document.getElementById("dinner-modal").style.display = "none"
-}
-
-function openSnackData(){
-    document.getElementById("snack-modal").style.display = "block"
-}
-
-function closeSnackData(){
-    document.getElementById("snack-modal").style.display = "none"
-}
