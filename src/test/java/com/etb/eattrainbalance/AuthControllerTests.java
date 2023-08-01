48bb93e8ff8a54759f5b420a698baefef5d5d8b8
ThePixelator0 <jordanjohnswork@gmail.com>
2023-07-31 19:13:37 -0700

    More Tests

---
+++ src/test/java/com/etb/eattrainbalance/AuthControllerTests.java
@@ -0,0 +1,71 @@
+package com.etb.eattrainbalance;
+
+import static org.mockito.BDDMockito.given;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.etb.eattrainbalance.controller.AuthController;
+import com.etb.eattrainbalance.modal.request.RegisterRequest;
+import com.etb.eattrainbalance.modal.response.AuthResponse;
+import com.etb.eattrainbalance.persistence.entity.User;
+import com.etb.eattrainbalance.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+
+@WebMvcTest(controllers = AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthService authService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    private User user;
+
+    @BeforeEach
+    public void init() {
+        user = User.builder().email("testEmail@sfu.ca").name("Test Name").password("TestPassword").build();
+    }
+
+    @Test
+    public void AuthController_Register_ReturnCreated() throws Exception {
+        AuthResponse expectedResponse = new AuthResponse();
+
+        given(authService.register(ArgumentMatchers.any(RegisterRequest.class), ArgumentMatchers.any(HttpServletRequest.class)))
+                .willReturn(expectedResponse);
+
+        mockMvc.perform(post("/api/auth/register")
+                .param("name", "Test Name")
+                .param("email", "testEmail@sfu.ca")
+                .param("password", "TestPassword"))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", "/dashboard"));
+    }
+
+
+
+}
