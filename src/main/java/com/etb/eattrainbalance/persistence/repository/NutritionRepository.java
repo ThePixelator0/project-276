2a69f1ad59fe84f989d1936fd2b51a6872d04b77
bevannutakor <bevannutakor@icloud.com>
2023-07-31 20:10:31 -0700

    aggregate data

--- src/main/java/com/etb/eattrainbalance/persistence/repository/NutritionRepository.java
+++ src/main/java/com/etb/eattrainbalance/persistence/repository/NutritionRepository.java
@@ -3,6 +3,8 @@ package com.etb.eattrainbalance.persistence.repository;
 import com.etb.eattrainbalance.persistence.entity.Nutrition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -10,4 +12,10 @@ import java.util.List;
 public interface NutritionRepository extends JpaRepository<Nutrition, Long> {
     List<Nutrition> findByUserIdAndMealType(Long userId, String mealType);
     List<Nutrition> findByUserId(Long userId);
+
+    // Get all Nutrition data from the past year for the given userId
+    List<Nutrition> findByUserIdAndCreationDateTimeAfter(Long userId, LocalDateTime date);
+
+    // Get all Nutrition data within a specific date range for the given userId
+    List<Nutrition> findByUserIdAndCreationDateTimeBetween(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 }
